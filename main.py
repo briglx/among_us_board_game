e473057a04213af2b9912e6cdbd548905b8538cf
Brig Lamoreaux <bklamoreaux@gmail.com>
2021-08-15 20:43:23 -0700

    Add assignments.

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ import random
 rooms = ["Bedroom", "Office", "Bathroom", "Kitchen", "Garage", "Back Patio", "Living Room", "Fancy Room"]
 players = ["Player 1", "Player 2", "Player 3", "Player 4"]
 assignment = ["Crew", "Crew", "Crew", "Imposter"]
+positions = {}
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 def draw_card():
-    room = rooms.pop(1)
+    room = rooms.pop(0)
     return room
     logging.info(room)
+
+def move_to_room(player, room):
+    # if player in positions:
+    #     positions[player] = room
+    # else:
+    #     positions[player] = room
+
+    positions[player] = room
+
 
 
 def main():
@@ -24,13 +34,23 @@ def main():
 
     logging.info("Shuffle Cards")
     random.shuffle(rooms)
+    logging.info(rooms)
 
     logging.info("Make Assignments")
     random.shuffle(assignment)
     logging.info(assignment)
 
-        
-    room = draw_card()
+    for player in players:    
+        room = draw_card()
+        move_to_room(player, room)
+
+    logging.info("Positions after Turn")
+    logging.info(positions)
+
+
+    logging.info("Remaining Cards")
+    logging.info(rooms)
+
     
 
 
@@ -40,4 +60,4 @@ if __name__ == "__main__":
 
     main()
     
-    logging.info(rooms)
+    
