3b0dd9010be0a78805ee381c81e2354846686edc
Brig Lamoreaux <bklamoreaux@gmail.com>
2021-08-16 18:00:45 -0700

    Add gameplay.

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # among_us_board_game
+
+
+# Rule Questions
+
+- Can an imposter kill when not their turn?
+    - On a crews turn they draw a room where the imposter is. Can the imposter kill as soon as they enter the room or wait until the imposters turn.
+    - Answer: Yes. If a crew walks into a room where the imposter the imposter can kill 
+
+# Development
+
+The development environment is to manages two types of environments:
+
+- Python Generator Code
+- Azure Resources
+
+## Python Generator code
+
+Setup your dev environment by creating a virtual environment
+
+```bash
+# virtualenv \path\to\.venv -p path\to\specific_version_python.exe
+python3 -m venv .venv .
+source .venv/bin/activate
+
+deactivate
+```
+
+## Style Guidelines
+
+This project enforces quite strict [PEP8](https://www.python.org/dev/peps/pep-0008/) and [PEP257 (Docstring Conventions)](https://www.python.org/dev/peps/pep-0257/) compliance on all code submitted.
+
+We use [Black](https://github.com/psf/black) for uncompromised code formatting.
+
+Summary of the most relevant points:
+
+- Comments should be full sentences and end with a period.
+- [Imports](https://www.python.org/dev/peps/pep-0008/#imports) should be ordered.
+- Constants and the content of lists and dictionaries should be in alphabetical order.
+- It is advisable to adjust IDE or editor settings to match those requirements.
+
+
+### Use new style string formatting
+
+Prefer [f-strings](https://docs.python.org/3/reference/lexical_analysis.html#f-strings) over ``%`` or ``str.format``.
+
+```python
+#New
+f"{some_value} {some_other_value}"
+# Old, wrong
+"{} {}".format("New", "style")
+"%s %s" % ("Old", "style")
+```
+
+One exception is for logging which uses the percentage formatting. This is to avoid formatting the log message when it is suppressed.
+
+```python
+_LOGGER.info("Can't connect to the webservice %s at %s", string1, string2)
+```
--- main.py
+++ main.py
@@ -6,56 +6,174 @@ import logging
 import sys
 import random
 
-rooms = ["Bedroom", "Office", "Bathroom", "Kitchen", "Garage", "Back Patio", "Living Room", "Fancy Room"]
-players = ["Player 1", "Player 2", "Player 3", "Player 4"]
-assignment = ["Crew", "Crew", "Crew", "Imposter"]
-positions = {}
+NUM_ROOM_CARDS = 2
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+rooms = ["Bedroom", "Office", "Bathroom", "Kitchen", "Garage", "Back Patio", "Living Room", "Fancy Room"]
+deck = []
+discard_pile = []
+players = ["Player 1", "Player 2", "Player 3", "Player 4"]
+cur_player_idx = 0
+ghosts = []
+assignment = ["Crew", "Crew", "Crew", "Imposter"]
+
+positions = {}
+imposter = None
+
+card_log = []
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+def create_deck(rooms):
+
+    for _ in range(NUM_ROOM_CARDS):
+        for room in rooms:
+            deck.append(room)
 
 def draw_card():
-    room = rooms.pop(0)
-    return room
-    logging.info(room)
+    card = deck.pop(0)
+    discard_pile.append(card)
+    card_log.append(card)
+    return card
 
 def move_to_room(player, room):
-    # if player in positions:
-    #     positions[player] = room
-    # else:
-    #     positions[player] = room
-
     positions[player] = room
 
 
+def is_imposter(player):
+    idx = players.index(player)
+    return assignment[idx] == "Imposter"
+
+def get_player_in_my_room(player):
+
+    targets = []
+
+    if player in positions:
+        my_room = positions[player]
+
+        for p, r in positions.items():
+            if r == my_room and p != player:
+                targets.append(p)
+
+    return targets
+        
+
+def kill_player(player):
+    if player not in ghosts:
+        ghosts.append(player)
+        positions[player] = "Grave"
+        
+
+def call_emergency_meeting():
+    if imposter and len(players) > 1:
+        positions[imposter] = "Space"
+
+def take_action(player):
+
+    if player in positions:
+
+        targets = get_player_in_my_room(player)
+        if len(targets) > 0:
+            if is_imposter(player):
+                if targets[0] != "Body":
+                    logging.debug("Imposter taking action.")          
+                    kill_player(targets[0])
+                    imposter = player
+                    logging.info(f"{player} killed {targets[0]}")
+            else:
+                if targets[0] == "Body":
+                    logging.debug("Crew taking action.")
+                    logging.info(f"{player} found a body!")
+                    if len(ghosts) > 0:
+                        call_emergency_meeting()
+
+        else:
+            logging.debug(f"No target in the room.") 
+
+    else:
+        logging.debug(f"{player} not in a room.")
+    
+
+def active_players():
+    global cur_player_idx
+
+    keep_searching = True
+    while keep_searching:
+
+        player = players[cur_player_idx]
+
+        keep_searching = player in ghosts 
+
+        # Increment index
+        cur_player_idx = cur_player_idx + 1
+        if cur_player_idx >= len(players):
+            cur_player_idx = 0  
+
+    yield player
 
 def main():
-    logging.info("Enter players.")
-    logging.info(players)
+    logging.debug("Enter players.")
+    logging.debug(players)
 
-    logging.info("Shuffle Cards")
-    random.shuffle(rooms)
-    logging.info(rooms)
+    logging.debug("Create Deck")
+    create_deck(rooms)
+    logging.debug("Shuffle Cards")
+    random.shuffle(deck)
+    logging.info(deck)
 
-    logging.info("Make Assignments")
+    logging.debug("Make Assignments")
     random.shuffle(assignment)
     logging.info(assignment)
 
-    for player in players:    
-        room = draw_card()
-        move_to_room(player, room)
-
-    logging.info("Positions after Turn")
-    logging.info(positions)
+    logging.debug("Add body to game")
+    move_to_room("Body", random.choice(rooms))
 
 
-    logging.info("Remaining Cards")
-    logging.info(rooms)
+    turn_count = 0
+    while len(deck) > 0:
 
+        logging.debug(f"Turn {turn_count}")
+
+        for player in active_players():   
+
+            # Take Action
+            take_action(player)
+
+            room = draw_card()
+            logging.info(f"{player} drew {room}.")
+            move_to_room(player, room)
+
+            # Take Action
+            take_action(player)
+        
+
+        logging.debug("Positions after Turn")
+        logging.debug(positions)
+
+
+        logging.debug("Remaining Cards")
+        logging.debug(deck)
+        turn_count = turn_count + 1
+
+        if imposter and (positions[imposter] == "Space"):
+            break
+
+
+    logging.info(f"Ghosts {ghosts}")
+
+    if imposter and (positions[imposter] == "Space"):
+        winner = "Crew!!"
+    if (len(players) == (len(ghosts) + 1)):
+        winner = "Imposter!!" 
+    else:
+        winner = "No Winner"
+
+    logging.info(f"{winner} with: \nplayers: {players}\nassignments: {assignment}\ndeck: {card_log}\npositions:  {positions}\nghosts: {ghosts}\nimposter: {imposter}")
     
 
 
+
 if __name__ == "__main__":
-    logging.info("Starting script")
+    logging.debug("Starting script")
     
 
     main()
