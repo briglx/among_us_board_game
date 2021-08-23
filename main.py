0d5a24946462e8c6fd02b0189b96eab0a71222c4
Brig Lamoreaux <bklamoreaux@gmail.com>
2021-08-23 14:15:39 -0700

    Use risk reward to choose room.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 
 from among_us import Simulation
 
-logging.basicConfig(filename="game.log", level=logging.DEBUG)
+logging.basicConfig(filename="game.log", level=logging.WARNING)
 
 if __name__ == "__main__":
     logging.debug("Starting script")
@@ -41,19 +41,20 @@ if __name__ == "__main__":
     BODY_LOCATION = "Back Patio"
     deck = ["Kitchen", "Kitchen", "Kitchen", "Kitchen", "Office", "Office"]
 
-    # for _ in range(1000):
+    for _ in range(1000):
 
-    #     import random
-    #     seed = random.randint(1, 100000000000)
+        import random
 
-    #     simulation = Simulation(seed=seed)
-    #     simulation.run()
+        seed = random.randint(1, 100000000000)
+
+        simulation = Simulation(seed=seed)
+        simulation.run()
 
     # simulation = Simulation(seed=31316319423)
     # simulation.run()
 
-    simulation = Simulation(seed=24162577240)
-    simulation.run()
+    # simulation = Simulation(seed=27730247269)
+    # simulation.run()
 
     # game = Game(seed=100,assignments=assignments,body_location=body_location,deck=deck )
     # game.game_loop()
